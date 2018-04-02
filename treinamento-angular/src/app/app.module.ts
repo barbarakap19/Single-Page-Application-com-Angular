2aa052e4fac4c4f684105084865779ee5b4965bf
barbarakap19 <barbara.andrade1901@gmail.com>
2018-04-02 10:56:36 -0300

    adicionando barra de navegação

--- treinamento-angular/src/app/app.module.ts
+++ treinamento-angular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PainelSimplesComponent
+    PainelSimplesComponent,
+    BarraNavegacaoComponent
     
   ],
   imports: [
