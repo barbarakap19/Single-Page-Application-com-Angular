e125e642e12a8e6d0b33cc9545c8b9f55ffdbcd5
barbarakap19 <barbara.andrade1901@gmail.com>
2018-04-02 09:00:20 -0300

    criado o componente painel-simples

--- treinamento-angular/src/app/app.module.ts
+++ treinamento-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PainelSimplesComponent } from './painel-simples/painel-simples.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelSimplesComponent
     
   ],
   imports: [
