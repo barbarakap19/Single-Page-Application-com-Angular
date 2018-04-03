84a34b25825cfae1f44a2566f6caae0209b42835
barbarakap19 <barbara.andrade1901@gmail.com>
2018-04-03 15:23:38 -0300

    Melhorando a view consulta

--- treinamento-angular/src/app/consulta/consulta.component.ts
+++ treinamento-angular/src/app/consulta/consulta.component.ts
@@ -18,11 +18,11 @@ export class ConsultaComponent implements OnInit {
   ],
 
   "marcadas" : [
-    { "especialidade" : "Cardiologia", "quantidade" : 20},
-    { "especialidade" : "Clínica Geral", "quantidade" : 30},
-    { "especialidade" : "Dermatologia", "quantidade" : 14},
-    { "especialidade" : "Gastroenterologia", "quantidade" : 10},
-    { "especialidade" : "Pediatria", "quantidade" : 13},
+    { "especialidade" : "Cardiologia", "quantidade" : 19},
+    { "especialidade" : "Clínica Geral", "quantidade" : 28},
+    { "especialidade" : "Dermatologia", "quantidade" : 13},
+    { "especialidade" : "Gastroenterologia", "quantidade" : 8},
+    { "especialidade" : "Pediatria", "quantidade" : 11},
 
   ]
 
