e9f735221198d5de03e364cbb54db612aa59559f
barbarakap19 <barbara.andrade1901@gmail.com>
2018-04-08 08:53:18 -0300

    Tratando erros na requisição

--- treinamento-angular/src/app/consulta/consulta.component.ts
+++ treinamento-angular/src/app/consulta/consulta.component.ts
@@ -11,12 +11,13 @@ export class ConsultaComponent implements OnInit, OnDestroy {
   
   consultas;
   consultaInscricao;
+  erro;
 
   constructor(private consultaService: ConsultaService) { }
 
   ngOnInit() {
-    this.consultaInscricao.this.consultaService.getConsultas()
-    .subscribe( dados => this.consultas = dados)
+    this.consultaInscricao = this.consultaService.getConsultas()
+    .subscribe( dados => this.consultas = dados, erro => this.erro = true)
   }
 
   ngOnDestroy() {
